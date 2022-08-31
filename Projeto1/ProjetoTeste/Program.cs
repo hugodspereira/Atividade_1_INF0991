88b2edcbcd7131a276bbe96c4e75bfa0e8162b19
Hugo Pereira <hugopereira.mg@gmail.com>
2022-08-31 07:50:14 -0300

    Alteração do Hugo

--- Projeto1/ProjetoTeste/Program.cs
+++ Projeto1/ProjetoTeste/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Alteracao do Hugo
+
+
+Console.WriteLine("Agora vai");
+//Alteracao do Hugo
+Console.WriteLine("Vamos lá!");
+//Alteracao do Hugo
+
+Console.WriteLine("Viva o Comtrole");
+Console.WriteLine("Agora vai");
+Console.WriteLine("Agora vai");
